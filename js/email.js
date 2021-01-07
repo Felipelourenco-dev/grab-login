594cd4d052be34ab899f31f9aaadc0dd9c4e858c
Felipe Lourenço <72045256+Felipelourenco-dev@users.noreply.github.com>
2021-01-07 19:31:31 -0300

    Update email.js

--- js/email.js
+++ js/email.js
@@ -1,1 +1,29 @@
 
+
+
+// Função que verifica se o email é valido e adiciona uma ação caso não seja //
+
+function validarEmail() {
+
+var email = document.querySelector('#email')
+var error = document.querySelector('.error')
+
+if(!email.checkValidity()) {
+
+    email.classList.add('invalido')
+    error.innerHTML = 'Por favor, insira um email válido'
+}
+}
+
+// Função para quando o input estiver ativo retirar a ação anterior
+
+function redefinirMsg() {
+    var error = document.querySelector('.error');
+
+    if(error.innerHTML == "Por favor, insira um email válido"){
+       
+        email.classList.remove('invalido')
+        error.innerHTML = " ";
+    }
+}
+
